38852fee621cfed7d6fb9721355bb654540bdead
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-08-15 10:34:29 +0300

    Expose twin local runtime

--- eRob_moveit/src/eRob_ROS2_MoveIt/erob_moveit_runtime/scripts/runtime_gateway/__init__.py
+++ eRob_moveit/src/eRob_ROS2_MoveIt/erob_moveit_runtime/scripts/runtime_gateway/__init__.py
@@ -1,4 +1,5 @@
 from runtime_gateway.base import RuntimeGateway
 from runtime_gateway.local import LocalRuntimeGateway
+from runtime_gateway.twin_local import TwinLocalRuntime
 
-__all__ = ["RuntimeGateway", "LocalRuntimeGateway"]
+__all__ = ["RuntimeGateway", "LocalRuntimeGateway", "TwinLocalRuntime"]
