a3476acb617e5d876fcdefbebf85cf7a242af48f
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-08-15 10:36:07 +0300

    Keep twin runtime import opt-in

--- eRob_moveit/src/eRob_ROS2_MoveIt/erob_moveit_runtime/scripts/runtime_gateway/__init__.py
+++ eRob_moveit/src/eRob_ROS2_MoveIt/erob_moveit_runtime/scripts/runtime_gateway/__init__.py
@@ -1,5 +1,4 @@
 from runtime_gateway.base import RuntimeGateway
 from runtime_gateway.local import LocalRuntimeGateway
-from runtime_gateway.twin_local import TwinLocalRuntime
 
-__all__ = ["RuntimeGateway", "LocalRuntimeGateway", "TwinLocalRuntime"]
+__all__ = ["RuntimeGateway", "LocalRuntimeGateway"]
