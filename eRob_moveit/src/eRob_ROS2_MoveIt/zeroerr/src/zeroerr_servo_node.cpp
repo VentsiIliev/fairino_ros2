56fd25cd71344293d0149e446529169a319e3d0a
Ventsislav Iliev <ventsislav.iliew@gmail.com>
2026-08-25 08:31:15 +0300

    reverted servo node

--- eRob_moveit/src/eRob_ROS2_MoveIt/zeroerr/src/zeroerr_servo_node.cpp
+++ eRob_moveit/src/eRob_ROS2_MoveIt/zeroerr/src/zeroerr_servo_node.cpp
@@ -236,10 +236,8 @@ private:
                         moveit_servo::KinematicState& current_state, const rclcpp::Time& cur_time,
                         bool use_trajectory)
   {
-    const auto status = servo_->getStatus();
-    const bool collision_halt_blocks_output =
-        status == moveit_servo::StatusCode::HALT_FOR_COLLISION && collision_checking_enabled_;
-    if (next_joint_state && status != moveit_servo::StatusCode::INVALID && !collision_halt_blocks_output)
+    if (next_joint_state && servo_->getStatus() != moveit_servo::StatusCode::INVALID &&
+        servo_->getStatus() != moveit_servo::StatusCode::HALT_FOR_COLLISION)
     {
       if (use_trajectory)
       {
